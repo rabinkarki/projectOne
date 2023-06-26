b396d9a294d557370905c071c6d1f361332530be
Rabin karki <rabinkarki970@gmail.com>
2023-06-26 23:26:08 +0545

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
 import Header from "./components/header.jsx";
 import Home from "./components/Home.jsx";
+import Footer from "./components/footer.jsx";
 function App() {
  
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
       </Routes>
+      <Footer />
     </Router>
   )
 }
--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import "../styles/footer.scss"
 
-export const footer = () => {
+export const Footer=()=> {
+
   return (
-    <div>footer</div>
+    <>
+     <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com/6packprogrammer" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/meabhisingh" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/meabhisingh" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+    </>
   )
 }
+export default Footer
